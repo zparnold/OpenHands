2fbe7349543c342a823c1d05299162b5e590ca48
Zach Arnold <me@zacharnold.org>
2026-02-05 08:57:54 -0500

    macos fixes

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -5,3 +5,6 @@ DOCKER_IMAGE=runtime
 # These variables will be appended by the runtime_build.py script
 # DOCKER_IMAGE_TAG=
 # DOCKER_IMAGE_SOURCE_TAG=
+
+DOCKER_IMAGE_TAG=oh_v1.3.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
+DOCKER_IMAGE_SOURCE_TAG=oh_v1.3.0_dqyva7oi0t8ezpny_qidpuu0icfie9y4m
